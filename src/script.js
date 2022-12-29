0553e6c19fa2d20ecafaaedcff3095a973f236be
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2022-12-28 22:02:10 -0600

    realistic finish

--- src/script.js
+++ src/script.js
@@ -28,26 +28,28 @@ const updateAllMaterials = () =>{
         if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial){
             // child.material.envMap = environmentMap
             child.material.envMapIntensity = debugObject.envMapIntensity
+            child.castShadow = true
+            child.receiveShadow = true
         }
     })
 }
 
 //environment map
 const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
+    '/textures/environmentMaps/3/px.jpg',
+    '/textures/environmentMaps/3/nx.jpg',
+    '/textures/environmentMaps/3/py.jpg',
+    '/textures/environmentMaps/3/ny.jpg',
+    '/textures/environmentMaps/3/pz.jpg',
+    '/textures/environmentMaps/3/nz.jpg'
 ])
-
+environmentMap.encoding = THREE.sRGBEncoding
 //apply env map to background
 scene.background = environmentMap
 //apply env map to all materials
 scene.environment = environmentMap
 
-debugObject.envMapIntensity = 5
+debugObject.envMapIntensity = .879
 gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(.001).onChange(()=>{
     updateAllMaterials()
 })
@@ -55,9 +57,9 @@ gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(.001).onChange(()=>{
 
 
 //Models
-gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf)=>{
-    gltf.scene.scale.set(10,10,10)
-    gltf.scene.position.set(0,-4,0)
+gltfLoader.load('/models/hamburger.glb', (gltf)=>{
+    gltf.scene.scale.set(.3,.3,.3)
+    gltf.scene.position.set(0,-1,0)
     gltf.scene.rotation.y = Math.PI * .75
     scene.add(gltf.scene)
 
@@ -68,8 +70,16 @@ gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf)=>{
 //light
 const light = new THREE.DirectionalLight(0xffffff, 3)
 light.position.set(.25, 3, -2.25)
+light.castShadow = true
+light.shadow.camera.far = 12
+light.shadow.mapSize.set(1024,1024)
+light.shadow.normalBias = .012
+
+// const DirectionalLightCameraHelper = new THREE.CameraHelper(light.shadow.camera)
+// scene.add(DirectionalLightCameraHelper)
 scene.add(light)
 
+gui.add(light.shadow, 'normalBias').min(0).max(.05).step(.001).name('normalBias')
 gui.add(light, 'intensity').min(0).max(10).step(.001).name('lightIntensity')
 gui.add(light.position, 'x').min(-5).max(5).step(.001).name('lightX')
 gui.add(light.position, 'y').min(-5).max(5).step(.001).name('lightY')
@@ -116,11 +126,26 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 3
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping
+})
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(.001)
 
 /**
  * Animate
